54a90c9c712b25f7687b98872d9824c14af6becd
thugzook <atachongnoah@gmail.com>
2021-09-15 00:06:37 -0400

    Added PostHTML to inject inline HTML into webpages

--- main.js
+++ main.js
@@ -1,6 +1,28 @@
 import {Map, View} from 'ol';
+import MousePosition from 'ol/control/MousePosition';
+import VectorLayer from 'ol/layer/Vector';
+import VectorSource from 'ol/source/Vector';
+import GeoJSON from 'ol/format/GeoJSON';
+import {Style, Fill, Stroke, Text} from 'ol/style';
+import Feature from 'ol/Feature';
 import TileLayer from 'ol/layer/Tile';
 import OSM from 'ol/source/OSM';
+import Icon from 'ol/style/Icon';
+import 'ol/ol.css';
+import {FullScreen, ScaleLine, defaults as defaultControls } from 'ol/control';
+import {createStringXY, toStringHDMS} from 'ol/coordinate';
+import Select, { SelectEvent } from 'ol/interaction/Select';
+import {fromLonLat, toLonLat} from 'ol/proj';
+import LineString from 'ol/geom/LineString';
+import { defaults, isEmpty } from 'lodash';
+import {defaults as defaultInteractions} from 'ol/interaction';
+import {click} from 'ol/events/condition';
+import {Circle as CircleGeom, Point} from 'ol/geom';
+
+const view = new View({
+  center: fromLonLat([-84.39, 33.77]),
+  zoom: 15
+});
 
 const map = new Map({
   target: 'map',
@@ -9,8 +31,5 @@ const map = new Map({
       source: new OSM()
     })
   ],
-  view: new View({
-    center: [0, 0],
-    zoom: 2
-  })
+  view: view
 });
